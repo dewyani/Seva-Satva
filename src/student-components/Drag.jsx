3022d0c3b116333b58c23068816cc2c5b151b207
neerja-1984 <nirjadoshi2003@gmail.com>
2023-11-06 21:09:55 +0530

    chale date pr?

--- src/student-components/Drag.jsx
+++ src/student-components/Drag.jsx
@@ -15,11 +15,11 @@ export default function Drag(props) {
     // console.log("coursename "+props.name + " id " +props.id)
     console.log(props.name + props.name.split(" ").length)
 
-    return(
+    return (
         // <li key={props.id} className="fillchoice--li" ref={drag} style={{border: isDragging? "2px solid red": "0px"}}>
         <li key={props.id} className="fillchoice--li" ref={drag} >
-        <img className={props.name.split(" ").length<2? "fillchoice--li-img":"fillchoice--li-img-two" } src={cookingImg} alt="course-img" />
-        <span className={props.name.split(" ").length<2? "fillchoice--li-span":"fillchoice--li-span-two" } >{props.name}</span>
-      </li>
+            <img className={props.name.split(" ").length < 2 ? "fillchoice--li-img" : "fillchoice--li-img-two"} src={cookingImg} alt="course-img" />
+            <span className={props.name.split(" ").length < 2 ? "fillchoice--li-span" : "fillchoice--li-span-two"} >{props.name}</span>
+        </li>
     )
 }
